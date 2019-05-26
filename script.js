82139b8fae9927d41307d2494a5d0b8544b64d5a
Vanoux <vanessa.roux891@orange.fr>
2019-05-26 17:15:07 +0200

    mailto + footer + reglage bug mobile

---
+++ script.js
@@ -0,0 +1,9 @@
+setTimeout(function(){
+    document.getElementById("loading").classList.add("animated");
+    document.getElementById("loading").classList.add("fadeOut");
+    setTimeout(function(){
+    document.getElementById("loading").classList.remove("animated");
+    document.getElementById("loading").classList.remove("fadeOut");
+    document.getElementById("loading").style.display = "none";
+    },800);
+},1500);
