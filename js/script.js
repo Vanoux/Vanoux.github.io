8afbdbb11c8114a6d939c18784f977b5f75e25be
Vanoux <vanessa.roux891@orange.fr>
2019-08-16 23:48:36 +0200

    MAJ site

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// $(function () { // This will wait for DOMReady
+//     $(document).on('scroll', function () {
+//         console.log("Scrolling.");
+//         $('.navbar').css('background-color', 'red');
+//     });
+// });
+
+
